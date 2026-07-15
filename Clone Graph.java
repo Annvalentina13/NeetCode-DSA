d971b190617868507b7cbb615c8f27db10caf2f6
Annie Valentina A <Annievalentina1307@gmail.com>
2026-07-15 21:39:23 +0530

    Update Clone Graph.java

--- Clone Graph.java
+++ Clone Graph.java
@@ -1,47 +1,41 @@
-Given a node in a connected undirected graph, return a deep copy of the graph.
-
-Each node in the graph contains an integer value and a list of its neighbors.
-
+/*
+Definition for a Node.
 class Node {
     public int val;
     public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 }
-The graph is shown in the test cases as an adjacency list. An adjacency list is a mapping of nodes to lists, used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+*/
 
-For simplicity, nodes values are numbered from 1 to n, where n is the total number of nodes in the graph. The index of each node within the adjacency list is the same as the node's value (1-indexed).
+class Solution {
 
-The input node will always be the first node in the graph and have 1 as the value.
+    private HashMap<Node, Node> map = new HashMap<>();
 
-Example 1:
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
 
+        if (map.containsKey(node))
+            return map.get(node);
 
+        Node clone = new Node(node.val);
+        map.put(node, clone);
 
-Input: adjList = [[2],[1,3],[2]]
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
 
-Output: [[2],[1,3],[2]]
-Explanation: There are 3 nodes in the graph.
-Node 1: val = 1 and neighbors = [2].
-Node 2: val = 2 and neighbors = [1, 3].
-Node 3: val = 3 and neighbors = [2].
-
-Example 2:
-
-
-
-Input: adjList = [[]]
-
-Output: [[]]
-Explanation: The graph has one node with no neighbors.
-
-Example 3:
-
-Input: adjList = []
-
-Output: []
-Explanation: The graph is empty.
-
-Constraints:
-
-0 <= The number of nodes in the graph <= 100.
-1 <= Node.val <= 100
-There are no duplicate edges and no self-loops in the graph.
+        return clone;
+    }
+}
